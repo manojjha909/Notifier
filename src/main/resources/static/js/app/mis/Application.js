f30aa91de8df0b7e9ced01c527fa0184e819082e
KhandekarMohammad Mohammad <v-kmohammad@expedia.com>
2017-08-01 00:57:50 +0530

    View

--- src/main/resources/static/js/app/mis/Application.js
+++ src/main/resources/static/js/app/mis/Application.js
@@ -11,7 +11,6 @@ $(document).ready(function () {
        window.location.href = 'login.html';
    }
 
-    var reminders;
     $.ajax({
         type: "POST",
         url: "/reminder/fetchReminders",
@@ -19,13 +18,37 @@ $(document).ready(function () {
         contentType: 'application/json',
         dataType: 'json',
         success: function(data) {
-            reminders = data;
+            console.log(data);
+            for(var i = 0; i < data.length; i++) {
+                var elem = "<div class=\"col-lg-3 col-md-6\"> " +
+                    "<div class=\"panel panel-primary\"> " +
+                    "<div class=\"panel-heading\"> " +
+                    "<div class=\"row\"> " +
+                    "<div class=\"col-xs-3\"> " +
+                    "<i class=\"fa fa-comments fa-5x\"></i> " +
+                    "</div> " +
+                    "<div class=\"col-xs-9 text-right\"> " +
+                    "<div class=\"huge\">26</div> " +
+                    "<div>" + data[i].reminderName + "</div> " +
+                    "</div> " +
+                    "</div> " +
+                    "</div> " +
+                    "<a href=\"#\"> " +
+                    "<div class=\"panel-footer\"> " +
+                    "<span class=\"pull-left\">View Details</span> " +
+                    "<span class=\"pull-right\"><i class=\"fa fa-arrow-circle-right\"></i></span> " +
+                    "<div class=\"clearfix\"></div> " +
+                    "</div> " +
+                    "</a> " +
+                    "</div> " +
+                    "</div>";
+
+                $('#reminderId').append(elem);
+
+            }
         }
     });
     
-    reminders.each(function (data) {
-        
-    })
     
     $('#logoutB').click(function () {
         Cookies.remove("user");
